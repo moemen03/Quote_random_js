9f4081d88d2b362c6299ff1693cf95a0cd0b07c2
moemen03 <89150107+moemen03@users.noreply.github.com>
2023-07-17 21:07:32 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,6 @@ var btn = document.querySelector(".button");
 var quotes=
 [
     "Be yourself; everyone else is already taken.",
-    "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
     "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
     "So many books, so little time.",
     "A room without books is like a body without a soul.",
@@ -25,7 +24,6 @@ var quotes=
 var autors=
 [
     "Oscar Wilde",
-    "Marilyn Monroe",
     "Albert Einstein",
     "Frank Zappa",
     "Marcus Tullius Cicero",
